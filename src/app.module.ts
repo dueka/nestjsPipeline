c75315f244784e668c5227ebc976749289193ba7
Ekanem David <38921132+dueka@users.noreply.github.com>
2022-10-11 14:28:44 +0100

    added unit tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ProductController } from "./product/controllers/product.controller";
+import { APP_FILTER } from "@nestjs/core";
+import { AuthController } from "./auth/auth.controller";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ProductModule } from "./product/product.module";
+import { AuthModule } from "./auth/auth.module";
+import { AllExceptionsFilter } from "./core/all-exceptions.filter";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true, // shouldn't be used in production - may lose data
+    }),
+    ProductModule,
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
 })
 export class AppModule {}
